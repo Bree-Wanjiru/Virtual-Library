2dc231189d1897b16a1b471302a04af21230356c
Brenda <ndirangubrenda44@gmail.com>
2023-04-25 20:46:08 +0300

    changes to js file

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ fetch(" http://localhost:3000/Books")
 .then(response =>response.json())
 .then(data =>{
     list.addEventListener("click",function(){
-        
+        data.map(function(dt){
+            const image=document.createElement("img")
+            image.src=dt.image
+            list.appendChild(image)
+            const book=document.createElement("li")
+            book.innerHTML=dt.name
+            list.appendChild(book)
+        })
     })
-    data.map(function(dt){
-        // const image=document.createElement("img")
-        // image.src=dt.image
-        // list.appendChild(image)
-        const book=document.createElement("li")
-        book.innerHTML=dt.name
-        list.appendChild(book)
-    })
+   
 })
