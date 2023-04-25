a23113dde36bb204932d94fdbccfc56d12907e04
Brenda <ndirangubrenda44@gmail.com>
2023-04-25 20:56:25 +0300

    changes to js and html

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const list=document.getElementById("list")
 fetch(" http://localhost:3000/Books")
 .then(response =>response.json())
 .then(data =>{
-    list.addEventListener("click",function(){
+    
         data.map(function(dt){
-            const image=document.createElement("img")
-            image.src=dt.image
-            list.appendChild(image)
+            
             const book=document.createElement("li")
             book.innerHTML=dt.name
             list.appendChild(book)
+            book.addEventListener("click",function(){
+
+            })
         })
-    })
+    
    
 })
