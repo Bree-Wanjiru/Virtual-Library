82ef18b8808352ac07ed1733e5b2de1e5fb05442
Brenda <ndirangubrenda44@gmail.com>
2023-04-25 20:22:30 +0300

    changes to db.json and js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const container=document.getElementsByClassName("books")
+const list=document.getElementById("list")
+fetch(" http://localhost:3000/Books")
+.then(response =>response.json())
+.then(data =>{
+    data.map(function(dt){
+        const book=document.createElement("li")
+        book.innerHTML=dt.name
+        list.appendChild(book)
+    })
+})
