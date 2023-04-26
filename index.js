e34844d3196845272bb4cd8db6070438a5ed8f6b
Brenda <ndirangubrenda44@gmail.com>
2023-04-26 07:17:30 +0300

    changes to json ,js and html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+//display books
 const container=document.getElementsByClassName("books")
 const list=document.getElementById("list")
 const poster=document.getElementById("image")
 const title=document.getElementById("title")
 const author=document.getElementById("author")
 const ISBN=document.getElementById("ISBN")
+
+//display authors
+const auth_list=document.getElementById("author_list")
+const name_auth=document.getElementById("author_name")
+const review_auth=document.getElementById("review_author")
 
 //add books
 const b_name=document.getElementById("e_bname")
@@ -12,6 +18,12 @@ const isbn=document.getElementById("e_isbn")
 const image=document.getElementById("e_image")
 const btn=document.getElementById("add-book")
 
+//add author
+const auth_name=document.getElementById("e_authname")
+const auth_review=document.getElementById("e_authreview")
+const button=document.getElementById("add-author")
+
+//get books
 fetch(" http://localhost:3000/Books")
 .then(response =>response.json())
 .then(data =>{
@@ -31,6 +43,8 @@ fetch(" http://localhost:3000/Books")
     
    
 })
+
+//post books
 btn.addEventListener("click",function(event){
     event.preventDefault()
         const new_book={
@@ -48,5 +62,44 @@ btn.addEventListener("click",function(event){
           })
             .then((response) => response.json())
             .then((new_book) => console.log(new_book))
+
+})
+
+//get authors
+fetch(" http://localhost:3000/Authors")
+.then(response =>response.json())
+.then(auth_data =>{
+    
+        auth_data.map(function(dt){
+            
+            const author=document.createElement("li")
+            author.innerHTML=dt.name
+            auth_list.appendChild(author)
+            author.addEventListener("click",function(){
+            name_auth.innerHTML=dt.author_name
+            review_auth.innerHTML=dt.author_review
+            })
+        })
+    
+   
+})
+
+//post authors
+button.addEventListener("click",function(event){
+    event.preventDefault()
+        const new_author={
+          author_name: auth_name.value,
+          author_review: auth_review.value,
+          
+        }
+        fetch("http://localhost:3000/Authors", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(new_author),
+          })
+            .then((response) => response.json())
+            .then((new_author) => console.log(new_author))
 
 })
