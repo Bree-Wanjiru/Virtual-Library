9758aaa989b5a5bfe6344326e88b372041aa2c24
Brenda <ndirangubrenda44@gmail.com>
2023-04-26 06:42:36 +0300

    changes to all files

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const title=document.getElementById("title")
 const author=document.getElementById("author")
 const ISBN=document.getElementById("ISBN")
 
+//add books
+const b_name=document.getElementById("e_bname")
+const a_name=document.getElementById("e_aname")
+const isbn=document.getElementById("e_isbn")
+const image=document.getElementById("e_image")
+const btn=document.getElementById("add-book")
 
 fetch(" http://localhost:3000/Books")
 .then(response =>response.json())
@@ -24,4 +30,23 @@ fetch(" http://localhost:3000/Books")
         })
     
    
+})
+btn.addEventListener("click",function(event){
+    event.preventDefault()
+        const new_book={
+          name: b_name.value,
+          author: a_name.value,
+          ISBN: isbn.value,
+          image: image.value
+        }
+        fetch("http://localhost:3000/Books", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(new_book),
+          })
+            .then((response) => response.json())
+            .then((new_book) => console.log(new_book))
+
 })
